fcd0fc771896d329620bc4abc9f6265e8953fcf5
Huy <truonggiahuy227@gmail.com>
2021-05-21 21:37:46 +0700

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/hello', (req, res) => {
   res.send('<h1>Hello World!</h1>')
